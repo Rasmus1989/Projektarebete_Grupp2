ec4634533131fc7abde0cdea856db3474d00457b
Sveako <knutssondavid94@gmail.com>
2015-05-12 13:12:45 +0200

    grön bakgrund

--- Test PieChart/src/Main.java
+++ Test PieChart/src/Main.java
@@ -6,9 +6,13 @@ public class Main {
 	
    public static void main(String[] argv) {
       JFrame frame = new JFrame();
-      frame.getContentPane().add(new PieChart());
+      frame.getContentPane().setBackground(Color.GREEN);
+      frame.getContentPane().setLayout(null);
+      PieChart pieChart = new PieChart();
+      pieChart.setBounds(0, 0, 509, 521);
+      frame.getContentPane().add(pieChart);
       frame.setBackground(Color.WHITE);
-      frame.setSize(500, 500);
+      frame.setSize(776, 568);
       frame.setVisible(true);
    }
 }
